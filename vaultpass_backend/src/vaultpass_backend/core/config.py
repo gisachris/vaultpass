8b08cc0ea70e04620892f6bf089aaa7d5d648a26
gdev <125574259+gisachris@users.noreply.github.com.>
2026-05-19 11:42:00 +0200

    ft: add app settings and config via pydantic-settings

--- vaultpass_backend/src/vaultpass_backend/core/config.py
+++ vaultpass_backend/src/vaultpass_backend/core/config.py
@@ -8,9 +8,9 @@ class Settings(BaseSettings):
 
     DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/vaultpass"
     
-    SECRET_KEY: str = "supersecretkeythatisnotsecuretokeepinversioncontrol"
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    JWT_SECRET_KEY: str = "supersecretkeythatisnotsecuretokeepinversioncontrol"
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
 
     model_config = SettingsConfigDict(
         env_file=".env", 
