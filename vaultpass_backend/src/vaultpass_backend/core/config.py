a7a77e0912377063cee22c31222d8cdc85313a7f
gdev <125574259+gisachris@users.noreply.github.com.>
2026-05-22 11:58:54 +0200

    ft: prepare configuration using pydantic settings

--- vaultpass_backend/src/vaultpass_backend/core/config.py
+++ vaultpass_backend/src/vaultpass_backend/core/config.py
@@ -1,10 +1,20 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "VaultPass_backend"
-    VERSION: str = "0.1.0"
+    PROJECT_NAME: str = "VaultPass API"
     API_V1_STR: str = "/api/v1"
+
+    DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/vaultpass"
     
-    model_config = SettingsConfigDict(case_sensitive=True)
+    SECRET_KEY: str = "supersecretkeythatisnotsecuretokeepinversioncontrol"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    model_config = SettingsConfigDict(
+        env_file=".env", 
+        env_file_encoding="utf-8",
+        case_sensitive=True
+    )
 
 settings = Settings()
