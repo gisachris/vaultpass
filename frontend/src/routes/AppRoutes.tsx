560422a1976a57f0a6f9095aac6ae2e0c398d3eb
gisa chris <125574259+gisachris@users.noreply.github.com>
2026-06-02 08:26:05 +0000

    ft: implement sign up and sign in pages

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from '../pages/Home';
+import { SignUpPage } from '../pages/SignUpPage';
+import { SignInPage } from '../pages/SignInPage';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/login" element={<SignInPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
